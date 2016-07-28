5fab9584b8d8b40e4e522d3b5d636915fac271f1
pmallya <pramod.mallya.1490@gmail.com>
2016-07-28 15:39:33 -0700

    Grey conversion change
    The grey conversion logic needed correction to make [2:0]^[3:1]

--- FIFO_Asynchronous_grey/fifo_grey_pointer.sv
+++ FIFO_Asynchronous_grey/fifo_grey_pointer.sv
@@ -14,7 +14,7 @@ module fifo_grey_pointer #(parameter FIFO_DEPTH=8)
 	else begin 
 		if(enable) begin
 			binary_count <= binary_count + 'd1;
-			grey_pointer <= {binary_count[$clog2(FIFO_DEPTH)-1] , binary_count[$clog2(FIFO_DEPTH)-2:0] ^ binary_count[$clog2(FIFO_DEPTH)-2:1]};
+			grey_pointer <= {binary_count[$clog2(FIFO_DEPTH)-1] , binary_count[$clog2(FIFO_DEPTH)-2:0] ^ binary_count[$clog2(FIFO_DEPTH)-1:1]};
 			end
 		else 
 			grey_pointer <= grey_pointer;
